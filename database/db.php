75dfe3fddb79dfc81c16c7fa580c0cd607dcc754
Phu Truong <tezukashuko@gmail.com>
2022-07-07 22:25:07 +0700

    complete setup

--- database/db.php
+++ database/db.php
@@ -5,5 +5,6 @@
     $db = "barbershop";
     $port = "3306";
     $conn = new mysqli($host, $user, $pw, $db, $port);
+    $conn_err = $conn->connect_errno;
     $conn->query("SET time_zone = '+07:00';") 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,20 @@
+<?php 
+include "./database/db.php";
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
+    <h1><?php if ($conn_err){
+        echo $conn->connect_error;
+    } 
+    else {
+        echo 'Connect DB complete';
+    }?></h1>
 </body>
 </html>
